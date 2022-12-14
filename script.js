ae1afe17f19b4be3efb7c527773223224f06db61
cheyenne.zeugin <e247154@edu.sbl.ch>
2022-12-14 14:18:00 +0100

    Aufgabe (4) b

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function animationLoop() {
     let cx2 = parseFloat(circle2.getAttribute("cx"))     
 
     // Start Loop
-    if (false) { // Aufgabe (4b)
+    if (cx1 ===  cx1 > cx2 + 10) { // Aufgabe (4b)
         ({v1, v2} = newVelocity(v1, v2))
     }
 
